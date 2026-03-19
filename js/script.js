da8e53887c1a2e4e141495af7f25babfb5a5d409
yurizyk_ <yuri.9624058@aluno.mg.gov.br>
2026-03-19 14:19:24 +0000

    Update JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 let likeCount = 0;
+let curtido = false; // flag booleana
 
 function curtir() {
-  likeCount++;
+
+if(curtido == false){
+likeCount++;
+curtido = true;
+  document.getElementById("likeCount").innerText = likeCount;
+} else{
+  likeCount--;
+  curtido = false;
   document.getElementById("likeCount").innerText = likeCount;
 }
-
+}
 document.getElementById("likeBtn").addEventListener("click", curtir);
