a6790a370131dfffa6efb8d257926a91222381e5
yurizyk_ <yuri.9624058@aluno.mg.gov.br>
2026-04-08 14:02:24 +0000

    yuri viado

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
 let likeCount = 0;
 let curtido = false; // flag booleana
 
+let deslikeCount = 0;
+let descurtido = false;
+
+// função de curtir
 function curtir() {
 
 if(curtido == false){
-likeCount++;
-curtido = true;
-  document.getElementById("likeCount").innerText = likeCount;
+   likeCount++;
+   curtido = true;
 } else{
-  likeCount--;
-  curtido = false;
-  document.getElementById("likeCount").innerText = likeCount;
+   likeCount--;
+   curtido = false;
 }
+
+document.getElementById("likeCount").innerText = likeCount;
 }
+
+// função de descurtir
+function descurtir(){
+
+if(descurtido == false){
+   deslikeCount++;
+   descurtido = true;
+} else{
+   deslikeCount--;
+   descurtido = false;
+}
+
+document.getElementById("deslikeCount").innerText = deslikeCount;
+}
+
+// eventos dos botões
 document.getElementById("likeBtn").addEventListener("click", curtir);
+document.getElementById("deslikeBtn").addEventListener("click", descurtir);
